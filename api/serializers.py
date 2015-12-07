7c730ddf9ac241e05318f98429f6d2bffa0c0f58
Peter Flynn <fredoflynn@gmail.com>
2015-12-07 11:46:58 -0800

    prep for heroku deploy

---
+++ api/serializers.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from users.models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """One to One with User
+    """
+    class Meta:
+        model = Profile
+        exclude = ('user',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'profile')
+        extra_kwargs = {'password': {'write_only': True}}
+        read_only_fields = ('id', )
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        profile_data = validated_data.pop('profile')
+        profile = Profile.objects.create(
+            user=user,
+            first_name=profile_data.get('first_name'),
+            last_name=profile_data.get('last_name'),
+            gender=profile_data.get('gender'),
+            age=profile_data.get('age')
+        )
+        return user
