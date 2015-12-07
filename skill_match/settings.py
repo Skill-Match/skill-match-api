535e731f544b5aca203f8befec88edf341ab75a0
Some Guy <abc@abc.com>
2015-12-07 11:55:15 -0800

    adding static files

--- skill_match/settings.py
+++ skill_match/settings.py
@@ -132,7 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static')
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.'
