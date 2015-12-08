f9b60b6a68d61e8ebf448ec63ae63250f6443b4f
Peter Flynn <fredoflynn@gmail.com>
2015-12-07 17:59:11 -0800

    adding fields to models

--- matchup/models.py
+++ matchup/models.py
@@ -8,9 +8,15 @@ class Park(models.Model):
 
 class Match(models.Model):
     creator = models.ForeignKey(User, related_name='creator')
-    players = models.ManyToManyField(User)
+    title = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField(max_length=1000, null=True, blank=True)
     park = models.ForeignKey(Park)
+    sport = models.CharField(max_length=25)
+    skill_level = models.IntegerField()
+    date_time = models.DateTimeField()
+    players = models.ManyToManyField(User)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 
 class Feedback(models.Model):
@@ -20,6 +26,6 @@ class Feedback(models.Model):
     park = models.ForeignKey(Park)
     skill = models.IntegerField()
     sportsmanship = models.IntegerField()
-    availablity = models.IntegerField()
+    availability = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
