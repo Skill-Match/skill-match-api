7032761ead17cee4f5ec0a4fa2a66639513a9fa8
Some Guy <abc@abc.com>
2015-12-10 14:01:31 -0800

    adding user profile page

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,14 @@
 from api.views import CreateUser, ListUsers, ListCreateMatches, ListCreateParks, \
     ListFeedbacks, CreateFeedbacks, DetailPark, DetailUpdateMatch, \
-    DetailFeedback, UpdateMatch
+    DetailFeedback, UpdateMatch, DetailUpdateUser
 from django.conf.urls import url
 from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r'^users/$', ListUsers.as_view(), name='api_list_users'),
     url(r'^users/create/$', CreateUser.as_view(), name='api_create_user'),
+    url(r'^users/(?P<pk>\d+)/', DetailUpdateUser.as_view(),
+        name='api_detail_update_user'),
     url(r'^api-token-auth/$', views.obtain_auth_token),
     url(r'^matches/$', ListCreateMatches.as_view(),
         name='api_list_create_matches'),
