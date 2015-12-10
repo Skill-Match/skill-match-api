ad479dcb648a0fa75c9cf3d276bedd3251d51d9b
Peter Flynn <fredoflynn@gmail.com>
2015-12-10 11:12:20 -0800

    adding command to grab yelp parks

---
+++ api/management/commands/add_parks.py
@@ -0,0 +1,68 @@
+from django.core.management import BaseCommand
+from datetime import date
+from matchup.models import Park
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        auth = Oauth1Authenticator(
+        consumer_key="J_lQomBe-LKch6QM6lp_4Q",
+        consumer_secret="uvPNl74cDNK23h9KeWWDPGCJ0rA",
+        token="Dxa-Yr4Zbf7dG52CHqxYAc5KpgDrdX62",
+        token_secret="8eJm_JtFMSkmt9hAWMYRp-uew3o"
+        )
+
+        params = {"category_filter": "parks"}
+        client = Client(auth)
+        r = client.search('89134', **params)
+        parks = r.businesses
+
+        count = 0
+        for park in parks:
+            park_exists = Park.objects.filter(name=park.name)
+            if len(park_exists) == 0:
+                d_list = park.location.display_address
+                d1 = d_list[0]
+                d2 = d_list[1]
+                if len(d_list) > 2:
+                    d3 = d_list[2]
+                else:
+                    d3 = None
+                Park.objects.create(
+                    rating=park.rating.rating,
+                    mobile_url=park.mobile_url,
+                    review_count=park.review_count,
+                    name=park.name,
+                    url=park.url,
+                    image_url=park.image_url,
+                    city=park.location.city,
+                    display_address1=d1,
+                    display_address2=d2,
+                    display_address3=d3,
+                    postal_code=park.location.postal_code,
+                    latitude=park.location.coordinate.latitude,
+                    longitude=park.location.coordinate.longitude,
+                    state_code=park.location.state_code
+                )
+                count += 1
+
+        self.stdout.write("{} Parks Added to Database".format(count))
+
+    # def handle(self, *args, **options):
+    #     """
+    #     This method is required to be overridden. It is the main source of code
+    #     :param args:
+    #     :param options:
+    #     :return:
+    #     """
+    #     count = 0
+    #     for wishlist in WishList.objects.all():
+    #         if wishlist.expiration <= date.today() and not wishlist.is_expired:
+    #             wishlist.close()
+    #             count += 1
+    #
+    #     self.stdout.write("{} Lists closed".format(count))
--- matchup/models.py
+++ matchup/models.py
@@ -4,7 +4,20 @@ from django.db import models
 
 
 class Park(models.Model):
-    name = models.CharField(max_length=50)
+    rating = models.FloatField(null=True, blank=True)
+    mobile_url = models.URLField(null=True, blank=True)
+    review_count = models.IntegerField(null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    image_url = models.URLField(null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    display_address1 = models.CharField(max_length=40, null=True, blank=True)
+    display_address2 = models.CharField(max_length=40, null=True, blank=True)
+    display_address3 = models.CharField(max_length=40, null=True, blank=True)
+    postal_code = models.CharField(max_length=10, null=True, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    state_code = models.CharField(max_length=5, null=True, blank=True)
 
     def __str__(self):
         return self.name
