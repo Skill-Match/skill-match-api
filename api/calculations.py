7510ce6bcd3c4be47d6ea36e0f8ce3fe2e61a1f6
Peter Flynn <fredoflynn@gmail.com>
2015-12-15 13:17:11 -0800

    refactor skill commands, add new command for entire all players update

--- api/calculations.py
+++ api/calculations.py
@@ -1,14 +1,29 @@
+from matchup.models import Feedback
 
-def new_skills(skill_object, sport):
+
+def calculate_skills(skill_object, sport):
     player = skill_object.player
-    pass
+    feedbacks = Feedback.objects.filter(match__sport=sport).\
+        filter(player=player)
+    skill_total = 0
+    sportsmanship_total = 0
+    count = 0
+    for feedback in feedbacks:
+        skill_total += feedback.skill
+        sportsmanship_total += feedback.sportsmanship
+        count += 1
+    skill_object.skill = skill_total / count
+    skill_object.sportsmanship = sportsmanship_total / count
+    skill_object.num_feedbacks = count
+    skill_object.save()
+
 
 
     # def handle(self, *args, **options):
     #     one_hour = timedelta(hours=1)
     #     new_feedbacks = Feedback.objects.filter(created_at__gt=
     #                                             (datetime.now() - one_hour))
-    #
+    #     num_updates = 0
     #     for feedback in new_feedbacks:
     #         player = feedback.player
     #         sport = feedback.match.sport
@@ -17,7 +32,6 @@ def new_skills(skill_object, sport):
     #         skill = Skill.objects.filter(player=player).filter(sport=sport)
     #
     #         #Update Skill every 3 matches to keep feedback anonymous
-    #         num_updates = 0
     #         if feedback_count % 3 == 0:
     #             if skill:
     #                 skill = skill[0]
---
+++ api/management/commands/update_all_skills.py
@@ -0,0 +1,26 @@
+from api.calculations import calculate_skills
+from django.core.management import BaseCommand
+from matchup.models import Feedback, Skill
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        feedbacks = Feedback.objects.all()
+
+        num_updates = 0
+        for feedback in feedbacks:
+            player = feedback.player
+            sport = feedback.match.sport
+            skill = Skill.objects.filter(player=player).filter(sport=sport)
+
+            if skill:
+                skill = skill[0]
+                calculate_skills(skill, sport)
+            else:
+                new_skill = Skill.objects.create(player=player, sport=sport)
+                calculate_skills(new_skill, sport)
+
+        self.stdout.write("Skills updated successfully".format(num_updates))
