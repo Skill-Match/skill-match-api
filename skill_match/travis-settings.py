bb9636259623f134123127831fda7c04fc48e5bb
Peter Flynn <fredoflynn@gmail.com>
2016-01-07 19:58:52 -0800

    trying again with travis

--- skill_match/travis-settings.py
+++ skill_match/travis-settings.py
@@ -1,5 +1,7 @@
 from skill_match.settings import *
 
+SECRET_KEY = 'vwn2!7ow-@azz(l#69k15&b15#)w4%b1f62m_8o&*s!clw(tk$'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
